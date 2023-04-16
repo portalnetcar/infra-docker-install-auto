7eb7024b691b87fcafa2927ba2918f10acad57e5
Leonardo Savio <leosavio@leosavio.com>
2023-04-16 01:51:51 -0300

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update package information and install prerequisites
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the stable Docker repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package information and install Docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable and start the Docker service
+#sudo systemctl enable docker
+#sudo systemctl start docker
+
+# Add the current user to the "docker" group
+sudo usermod -aG docker $USER
+
+# Test install 
+docker run hello-world
+
+# Print a message indicating the installation was successful
+echo "Docker installation is complete. You may need to log out and log back in for the changes to take effect."
